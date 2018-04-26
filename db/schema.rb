edb30f7f37a4e66a08f926f3ac93ce07a64520ae
Kaustubh Nair <kaustubh.nair108@gmail.com>
2018-04-26 15:03:21 +0530

    minor tag chnge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423140711) do
+ActiveRecord::Schema.define(version: 20180425120950) do
 
   create_table "contributions", force: :cascade do |t|
     t.string "contribution_type"
