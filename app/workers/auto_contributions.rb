2e3cc5ba28246aef32bd6ef7646a6cbf2939a2fb
Kaustubh Nair <kaustubh.nair108@gmail.com>
2018-06-08 16:45:24 +0530

    run job now

--- app/workers/auto_contributions.rb
+++ app/workers/auto_contributions.rb
@@ -1,13 +1,15 @@
-class FetchContributionsJob
+require 'sidekiq/api'
+ 
+class FetchContributionsWorker
   include Sidekiq::Worker
   def perform()
     Sidekiq::ScheduledSet.new.clear
-    Fetch.perform_in(1.days)
+    FetchContributionsWorker.perform_in(1.days)
     FetchGithubContribution.new.get_github_contributions
   end
 end
 
-FetchContributionsJob.perform_in(1.days)
+FetchContributionsWorker.perform_in(10.seconds)
 
 
 
